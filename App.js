70fef24e5b9f0be8c8853fc2b15bbbce4c22143c
Alberto Álvarez <alber.alvagon@alum.uca.es>
2024-03-09 14:45:41 +0100

    Adding date calendar to date input and input suggestions

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import { Provider as PaperProvider } from 'react-native-paper'; // Importa el Provider de react-native-paper
-import SearchMenu from './src/components/SearchMenu'; // Importa tu componente SearchMenu
+import React from "react";
+import { Provider as PaperProvider } from "react-native-paper"; // Importa el Provider de react-native-paper
+import { StyleSheet } from "react-native"; // Importa StyleSheet de react-native
+import SearchMenu from "./src/components/SearchMenu"; // Importa tu componente SearchMenu
+import { View } from "react-native";
+
 
 const App = () => {
   return (
-    <PaperProvider>
-      <SearchMenu />
-    </PaperProvider>
+    
+      <PaperProvider>
+        <SearchMenu />
+      </PaperProvider>
+    
   );
 };
+
+
 
 export default App;
--- src/components/SearchMenu.jsx
+++ src/components/SearchMenu.jsx
@@ -1,65 +1,131 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableWithoutFeedback, Button } from 'react-native';
-import { TextInput, Menu } from 'react-native-paper'; // Importa Menu de react-native-paper
+import React, { useState, useRef } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  TouchableWithoutFeedback,
+  FlatList,
+} from "react-native";
+import { TextInput, Menu } from "react-native-paper";
+import { Calendar } from "react-native-calendars";
 
-// Importa el JSON
-import nucleosData from '../assets/Nucleos_Origen_Destino.json';
+import nucleosData from "../assets/Nucleos_Origen_Destino.json";
 
 const SearchMenu = () => {
-  const [destination, setDestination] = useState('');
-  const [date, setDate] = useState('');
+  // Hooks
+  const [destination, setDestination] = useState("");
+  const [date, setDate] = useState("");
   const [visible, setVisible] = useState(false);
+  const [showCalendar, setShowCalendar] = useState(false);
+  const calendarRef = useRef(null);
+  const [suggestions, setSuggestions] = useState([]);
 
   const handleSearch = () => {
-    //  Lógica para buscar autobuses según el destino y la fecha
-    console.log('Buscar autobuses con destino:', destination, 'y fecha:', date);
+    console.log("Buscar autobuses con destino:", destination, "y fecha:", date);
   };
 
+  const handleDateSelect = (selectedDate) => {
+    setDate(selectedDate.dateString);
+    setShowCalendar(false);
+  };
+
+  const handleCalendarPress = () => {
+    setShowCalendar(true);
+  };
+
+  // Función para filtrar las sugerencias de paradas de autobús
+  const filterSuggestions = (text) => {
+    const filteredSuggestions = nucleosData.nucleos.filter((item) =>
+      item.toLowerCase().includes(text.toLowerCase())
+    );
+    setSuggestions(filteredSuggestions);
+  };
+
+  // Función para manejar el cambio en el campo de entrada
+  const handleInputChange = (text) => {
+    setDestination(text);
+    filterSuggestions(text);
+  };
+
+  // Función para manejar la selección de una sugerencia
+  const handleSuggestionSelect = (item) => {
+    setDestination(item);
+    setSuggestions([]);
+  };
+
+  // Renderizado de elementos de sugerencia
+  const renderSuggestion = ({ item }) => (
+    <Text
+      style={styles.suggestionItem}
+      onPress={() => handleSuggestionSelect(item)}
+    >
+      {item}
+    </Text>
+  );
+
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.title}>Bus Puerto</Text>
-        <Text style={styles.subtitle}>Introduce tu destino.</Text>
+    <TouchableWithoutFeedback
+      onPress={() => {
+        setShowCalendar(false);
+      }}
+    >
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.title}>Bus Puerto</Text>
+          <Text style={styles.subtitle}>Introduce tu destino.</Text>
+        </View>
+        <View style={styles.inputs}>
+          <TextInput
+            style={styles.input}
+            placeholder="Introduce núcleo destino"
+            value={destination}
+            onChangeText={handleInputChange}
+          />
+
+          <FlatList
+            data={suggestions}
+            renderItem={renderSuggestion}
+            keyExtractor={(item, index) => index.toString()}
+            style={styles.suggestionsContainer}
+          />
+
+          <Pressable onPress={handleCalendarPress}>
+            <TextInput
+              style={styles.input}
+              label="Fecha"
+              placeholder="Fecha"
+              value={date}
+              editable={false}
+            />
+          </Pressable>
+          {showCalendar && (
+            <View ref={calendarRef}>
+              <Calendar
+                onDayPress={handleDateSelect}
+                markedDates={{
+                  [date]: {
+                    selected: true,
+                    marked: true,
+                    selectedColor: "#000",
+                  },
+                }}
+              />
+            </View>
+          )}
+          <Pressable style={styles.button} onPress={handleSearch}>
+            <Text style={styles.button_text}>Buscar</Text>
+          </Pressable>
+        </View>
       </View>
-      <View style={styles.inputs}>
-        <Menu
-          visible={visible}
-          onDismiss={() => setVisible(false)}
-          anchor={
-            <TouchableWithoutFeedback onPress={() => setVisible(true)}>
-              <View>
-                <TextInput
-                  style={styles.input}
-                  label="Seleccionar destino"
-                  value={destination}
-                  editable={false} // Para desactivar la edición del campo de texto
-                />
-              </View>
-            </TouchableWithoutFeedback>
-          }
-        >
-          {nucleosData.nucleos.map((item, index) => (
-            <Menu.Item key={index} onPress={() => { setDestination(item); setVisible(false); }} title={item} />
-          ))}
-        </Menu>
-        <TextInput
-          style={styles.input}
-          label="Fecha"
-          placeholder="Fecha"
-          value={date}
-          onChangeText={text => setDate(text)}
-        />
-         <Button style={styles.button} title="Buscar" onPress={handleSearch} />
-      </View>
-      {/* Aquí puedes renderizar los detalles de los autobuses */}
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f0f0f0',
+    backgroundColor: "#f0f0f0",
     padding: 20,
   },
   header: {
@@ -67,22 +133,46 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 5,
   },
   subtitle: {
     fontSize: 16,
-    color: 'gray',
+    color: "gray",
   },
   inputs: {
     marginBottom: 20,
   },
   input: {
     marginBottom: 10,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 10,
+  },
+  suggestionsContainer: {
+    maxHeight: 100,
+  },
+  suggestionItem: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
   },
   button: {
-    marginBottom: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
     borderRadius: 20,
+    elevation: 3,
+    backgroundColor: "#000",
+  },
+
+  button_text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
   },
 });
 
