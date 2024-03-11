0c87b065324b4b586a5807d009532b394ccf6f24
Alberto Álvarez <alber.alvagon@alum.uca.es>
2024-03-11 14:27:51 +0100

    Testing Navigation Drawer

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,33 +1,41 @@
-import React from "react";
-import { View, Text, StyleSheet, StatusBar } from "react-native";
-import { Drawer } from "react-native-paper";
-const Navbar = () => {
-  
+import * as React from 'react';
+import { Button, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+function HomeScreen({ navigation }) {
   return (
-    
-    <View style={styles.navbar}>
-      <Text style={styles.navbarText}>Puerto Bus</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
     </View>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  navbar: {
-    backgroundColor: "#009AF0", // Color de fondo de la barra de navegación
-    height: 60 + StatusBar.currentHeight, // Altura de la barra de navegación con la altura de la barra de estado
-    justifyContent: "center", // Alineación vertical al centro
-    alignItems: "center", // Alineación horizontal al centro
-    position: "absolute", // Posicionamiento absoluto
-    top: 0, // Alinear en la parte superior de la pantalla
-    left: 0, // Alinear a la izquierda de la pantalla
-    right: 0, // Alinear a la derecha de la pantalla
-    paddingTop: StatusBar.currentHeight, // Ajustar el relleno superior para evitar que el contenido se superponga con la barra de estado
-  },
-  navbarText: {
-    color: "white", // Color del texto de la barra de navegación
-    fontSize: 20, // Tamaño del texto de la barra de navegación
-    fontWeight: "bold", // Peso del texto de la barra de navegación
-  },
-});
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
 
-export default Navbar;
+const Drawer = createDrawerNavigator();
+
+export default function NavBar() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerTitle: () => null }} // Ocultar el título en la barra de navegación
+        />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} 
+        options={{ headerTitle: () => null }}/>
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
